e06da3d332ad27075dd53c74f81c2e03bd45318b
martinyg <martinyg@mail.uc.edu>
2023-02-07 01:32:19 +0000

    added more complex count me

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -9,7 +10,11 @@ def hello_world():
 
 @app.route('/countme/<input_str>')
 def count_me(input_str):
-  return input_str
+  input_counter = Counter(input_str)
+  response = []
+  for letter, count in input_counter.most_common():
+    response.append('"{}": {}'.format(letter, count))
+  return '<br>'.join(response)
 
 if __name__ == '__main__':
   app.run()
